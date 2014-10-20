9a33aad053b0692b44e2a7d787c75c2fb42c3d85
Dav Clark <davclark@berkeley.edu>
2014-10-20 12:06:31 -0700

    Updated mongodb docker script, "works" but not really

--- mongodb/full-docker-mongo.sh
+++ mongodb/full-docker-mongo.sh
@@ -1,18 +1,17 @@
 # -p creates intermediate directories, and also doesn't complain if the dir
 # already exists
-DB_DIR=/var/mongo/cwBerkeley
-sudo mkdir -p $DB_DIR/db
-sudo chown -R $USER $DB_DIR
+DB_DIR=$(pwd)/data
+mkdir -p $DB_DIR/db
 
 # XXX - now we need to map the above into the VirtualBox boot2docker VM
 
 # We should perhaps check for a pre-existing DOCKER_HOST var,
 # and also make sure we get a successful exit code
 # Also, this may rely on Docker >= 1.2
-eval $(boot2docker shellinit)
+$(boot2docker shellinit)
 export DOCKER_HOST
 
 docker run -d -p 27017:27017 -p 28017:28017 \
-    -v /var/mongo/cwBerkeley:/data \
+    -v $DB_DIR:/data \
     --name mongodb \
     dockerfile/mongodb mongod --rest --httpinterface
